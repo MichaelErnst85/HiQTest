a162ac72a600fa26d929c371799a81f72a78b57c
Michael Ernst <Michael_Ernst@hotmail.com>
2021-06-07 21:27:38 +0200

    added migration and DbContext

---
+++ TextreaderAPI/TextreaderAPI/Models/File.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TextreaderAPI.Models
+{
+    
+    public class File
+    {
+        [Key]
+        public int id { get; set; }
+        [Column(TypeName = "varchar(200)")]
+        public string fileName { get; set; }
+
+
+    }
+}
--- TextreaderAPI/TextreaderAPI/Startup.cs
+++ TextreaderAPI/TextreaderAPI/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TextreaderAPI.Data;
 
 namespace TextreaderAPI
 {
@@ -26,6 +28,9 @@ namespace TextreaderAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<TextreaderAPIContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("TextreaderAPIContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
